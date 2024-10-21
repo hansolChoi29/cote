20beb5d0e1bd89921102d2f552bb96cfb09801e9
choi_hansol <ggksthf29@gmail.com>
2024-10-21 09:28:31 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,16 @@
+// function solution(num1,  num2){
+//     if(num1==num2){
+//     return 1;
+//     }else{
+//     return -1;
+//     }
+// }
+
+function solution(num1, num2){
+    if(num1==num2){
+        return 1
+    }else{
+        return -1
+    }
+
+}
