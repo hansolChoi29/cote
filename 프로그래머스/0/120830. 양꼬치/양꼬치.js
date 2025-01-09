9a5afefeb14b02e27e256d471cbaf0d98c1fd784
Hansol-Choi <ggksthf29@gmail.com>
2025-01-09 10:06:14 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,8 +1,3 @@
-function solution(n, k) {
-    return n*12000+k*2000-(Math.floor(n/10)*2000)
+function solution(n,k){
+    return n*12000+k*2000-Math.floor(n/10)*2000
 }
-
-//양꼬치*12000+음료수*2000
-//만약 n%10==0이면 k-1
-
-//n*12000-(1*2000)
