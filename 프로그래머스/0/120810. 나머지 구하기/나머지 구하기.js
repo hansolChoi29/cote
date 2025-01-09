04bd768c18c087de623df0a7f72b8d7e49f00609
Hansol-Choi <ggksthf29@gmail.com>
2025-01-09 09:34:33 +0900

    [level 0] Title: 나머지 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.js
@@ -0,0 +1,23 @@
+// // function solution(num1, num2){
+// //     let answer=num1%num2;
+// //     return answer
+// // }
+
+// // const sum=(num1, num2)=>{
+// //     return num1+num2;
+// // }
+// // const result=sum(10,20);
+// // console.log(result);
+
+// function solution(num1, num2){
+//     let answer=num1%num2;
+//     return answer;
+// }
+
+
+
+
+function solution(num1, num2){
+    let answer=num1%num2
+    return answer;
+}
