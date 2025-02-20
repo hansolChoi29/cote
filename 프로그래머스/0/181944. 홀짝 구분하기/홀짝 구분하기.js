276449592659dbc14e2308b75df90ca9f19eaa7b
HansolChoi <ggksthf29@gmail.com>
2025-02-20 22:24:59 +0900

    [level 0] Title: 홀짝 구분하기, Time: 66.48 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    n = Number(input[0]);
+    let result='odd'
+    if(n%2===0){
+        result='even'
+    }
+    console.log(`${n} is ${result}`)
+});
