17d935bbfa20df059cccd3523921d865d2111d8f
hansolChoi29 <ggksthf29@gmail.com>
2025-02-25 23:31:14 +0900

    chore:변수명 변경

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,7 +1,7 @@
 function solution(num_list) {
-    const 합제곱=num_list.reduce((acc,cur)=>acc+cur ,0)
-    const 곱=num_list.reduce((acc, cur)=>acc*cur,1)
-    return 곱<합제곱*합제곱?1:0
+  const 합 = num_list.reduce((acc, cur) => acc + cur, 0);
+  const 곱 = num_list.reduce((acc, cur) => acc * cur, 1);
+  return 곱 < 합 * 합 ? 1 : 0;
 }
 // 모든 원소들의 곱이 <
 // 모든 원소들의 합의 제곱 ?1:0
