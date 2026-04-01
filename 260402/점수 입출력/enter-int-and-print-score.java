0fa376e8b31a80b73fd7938e610da09bcdc7313a
최한솔 <ggksthf29@gmail.com>
2026-04-02 08:41:35 +0900

    Title: 점수 입출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String  사용자입력 = sc.next();
+        System.out.printf("Your score is %s point.", 사용자입력);
+    }
+}
