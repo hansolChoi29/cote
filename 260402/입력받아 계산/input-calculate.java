affc718722b84be673b2e283c85d47646d6376da
최한솔 <ggksthf29@gmail.com>
2026-04-02 08:45:19 +0900

    Title: 입력받아 계산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/입력받아 계산/input-calculate.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        // 정수 a, a에 +2 출력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("%d", a + 2);
+    }
+}
