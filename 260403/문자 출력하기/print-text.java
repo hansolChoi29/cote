b8333b322509513b37413c9fe89b5cefda0ef00b
최한솔 <ggksthf29@gmail.com>
2026-04-03 08:06:02 +0900

    Title: 문자 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/문자 출력하기/print-text.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+         String result = sc.next();
+
+         for(int i = 0; i<=7; i++){
+            System.out.printf(result);
+         }
+    }
+}
