2c5c54e82c0aa02c43f82e108a9f42cc2e84ef5e
최한솔 <ggksthf29@gmail.com>
2026-04-03 08:22:07 +0900

    Title: 차례로 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/차례로 출력/print-in-order.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        // 5~17 공백사이 엥?
+        for(int i =5; i <=17; i++){
+            System.out.printf(i+ " ");
+        }
+    }
+}
