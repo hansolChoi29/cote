6052ce990e431fe0bc07d2a8d4e4e299676ad77f
최한솔 <ggksthf29@gmail.com>
2026-04-02 09:02:47 +0900

    Title: 입력받은 값 교체하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/입력받은 값 교체하기/changing-inputs.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Please write your code here.
+        // 정수 a, b 위치 교환 후 출력
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.printf("%d %d", b, a);
+    }
+}
