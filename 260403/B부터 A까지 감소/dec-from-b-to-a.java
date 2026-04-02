7fd13b7c6144ca4c322ff054e9787a0cee8b7cc7
최한솔 <ggksthf29@gmail.com>
2026-04-03 08:40:00 +0900

    Title: B부터 A까지 감소, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260403/B부터 A까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        for(int i = B; B >= A; i--){
+            System.out.printf(B + " ");
+            B--;
+        }
+    }
+}
