db48c67093ce6b56d7d56266653b44a546b50daf
최한솔 <ggksthf29@gmail.com>
2026-04-04 09:39:32 +0900

    Title: 중앙값 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260404/중앙값 구하기/find-the-median.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int arr [] ={A, B, C};
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
