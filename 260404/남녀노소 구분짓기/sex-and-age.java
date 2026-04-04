30e93fc1f1e8edf78477b74c7a816b9487539ba8
최한솔 <ggksthf29@gmail.com>
2026-04-04 09:11:27 +0900

    Title: 남녀노소 구분짓기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260404/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        // 성별과 나이 입력 받기
+        // MAN 성인남자
+        // WOMAN 성인 여자
+        // BOY 미성년남자
+        // GIRL 미성년여자
+        Scanner sc = new Scanner(System.in);
+        int 성별 = sc.nextInt(); // 남자 -> 0, 여자 -> 1
+        int 나이 = sc.nextInt();
+        if(성별 == 0){
+            if(나이 >= 19){
+                System.out.println("MAN");
+            }else{
+                System.out.println("BOY");
+            }
+        }else{
+            if(나이 >= 19){
+                System.out.println("WOMAN");
+            }else{
+                System.out.println("GIRL");
+            }
+        }
+    }
+}
