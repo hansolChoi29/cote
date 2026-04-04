7b28cb912b796da92f08f078034b77a05f8931b0
최한솔 <ggksthf29@gmail.com>
2026-04-04 09:31:46 +0900

    Title: 윤년인가, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260404/윤년인가/is-leap-year.java
+++ 260404/윤년인가/is-leap-year.java
@@ -8,14 +8,8 @@ public class Main {
         // 윤년인지 판단하는 프로그램
         //  Y % 4 == 0 ? 윤년 : 평년
         // 100으로 나누어 떨어져도 400으로 나누어 떨어지지 않는 해는 평년
-        if(Y % 400 == 0){
-            System.out.println("true");
-        }else if(Y % 100 == 0){
-            System.out.println("false");
-        }else if(Y % 4 == 0){
-            System.out.println("true");
-        }else{
-            System.out.println("false");
-        }
+        // 400의 배수이거나, 100의 배수아니면서 4의 배수
+        boolean isLeap = (Y % 400 == 0) || (Y % 100 != 0 && Y % 4 ==0);
+        System.out.println(isLeap ? "true":"false");
     }
 }
